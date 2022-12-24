371626fb28c77bd9badc5eb1923cae01d7bb0d9b
Brandon Cloutier <brandoncloutier412@gmail.com>
2022-12-23 16:32:31 -0800

    part 1.5 creating a course object and fixing child components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,27 +1,29 @@
 import React from "react"
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
@@ -29,16 +31,16 @@ const App = () => {
 const Header = (props) => {
   return (
   <React.Fragment>
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   </React.Fragment>)
 }
 
 const Content = (props) => {
   return (
     <React.Fragment>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
     </React.Fragment>
   )
 }
@@ -54,7 +56,7 @@ const Part = (props) => {
 const Total = (props) => {
   return (
     <React.Fragment>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
     </React.Fragment>
   )
 }
