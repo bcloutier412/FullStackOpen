d00fe395fac0a9132f50d1775c88f8d264470cfa
Dino <brandoncloutier412@gmail.com>
2023-01-05 16:29:49 -0800

    finished img axios request

--- part2c-tasks/src/components/Weather.js
+++ part2c-tasks/src/components/Weather.js
@@ -1,34 +1,30 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const api_key = process.env.REACT_APP_API_KEY
+
 const Weather = ({ country }) => {
     const [weather, setWeather] = useState('')
-    const [temperature, setTemperature] = useState('')
-    const [wind, setWind] = useState('')
-    const [img, setImg] = useState('')
 
     useEffect(() => {
         axios
-            .get(`http://api.openweathermap.org/geo/1.0/direct?q=${country.capital[0]}&limit=5&appid=e6cdb4b225ca621b48c2686f4b98a9c5`)
+            .get(`http://api.openweathermap.org/geo/1.0/direct?q=${country.capital[0]}&limit=5&appid=${api_key}`)
             .then((response) => {
-                return axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${response.data[0].lat}&lon=${response.data[0].lon}&appid=e6cdb4b225ca621b48c2686f4b98a9c5&units=imperial`)
+                return axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${response.data[0].lat}&lon=${response.data[0].lon}&appid=${api_key}&units=imperial`)
             })
             .then((response) => {
-                setTemperature(response.data.main.temp)
-                setWind(response.data.wind.speed)
-                return axios.get(`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
-            }).then((response) => {
-                
-            }).finally(() => {
-                setWeather({temperature: temperature, wind: wind, img: 'data:image/png;base64,'+img})
+                const temperature = response.data.main.temp
+                const wind = response.data.wind.speed
+                const img = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+                setWeather({temperature: temperature, wind: wind, img: img})
             })
-    }, [country, temperature, wind, img])
+    }, [country])
     return (
         <div>
             {weather
             ? <div>
                 <p>temperature: {weather.temperature}</p>
-                <img src={weather.img}/>
+                <img src={weather.img} alt={`Weather in ${country.capital[0]}`}/>
                 <p>wind: {weather.wind}</p>
               </div>
             : <div>Loading...</div>} 
