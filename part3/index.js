3bd64b799843583832206e31b7ded2fe9047a264
Brandon Cloutier <brandoncloutier412@gmail.com>
2023-01-19 17:31:07 -0800

    added mongodb database to the note application

--- part3/index.js
+++ part3/index.js
@@ -1,38 +1,18 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const cors = require('cors')
+const Note = require('./models/note')
 
 app.use(express.json());
 app.use(cors())
 app.use(express.static('build'))
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true,
-  },
-];
-
-// app.get("/", (req, res) => {
-//   res.send("<h1>Hello World!</h1>");
-// });
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  Note.find({}).then(result => {
+    res.send(result);
+  })
 });
 
 app.get("/api/notes/:id", (request, response) => {
