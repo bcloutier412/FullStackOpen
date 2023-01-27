16a5287e1e0edcaa452bdbe0b7b949c4bb39e982
Dino <brandoncloutier412@gmail.com>
2023-01-27 11:33:35 -0800

    finished full mongodb route implementation

--- part3-application/server/index.js
+++ part3-application/server/index.js
@@ -2,21 +2,52 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 const Person = require("./models/person");
+
+/*
+ * MIDDLEWARE
+ */
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 app.use(express.json());
 app.use(cors());
+app.use(requestLogger);
 app.use(express.static("build"));
 
-persons = [];
-
+/*
+ * ROUTES
+ */
 app.get("/api/persons", (request, response) => {
-  Person.find({}).then((people) => {
-    response.send(people);
-  });
+  Person.find({})
+    .then((people) => {
+      response.send(people);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (request, response, next) => {
+  console.log("here");
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
@@ -28,12 +59,9 @@ app.get("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
-  return maxId + 1;
-};
 app.post("/api/persons", (request, response) => {
   const body = request.body;
+
   // Check valid input
   if (!body.name || !body.number) {
     return response
@@ -46,51 +74,68 @@ app.post("/api/persons", (request, response) => {
    * If it is a new user then we will save the new user to the database
    * we had to put everything into the .then because we need to wait for the database response before proceeding
    */
-  Person.find({ name: { $eq: body.name } }).then((personInDatabase) => {
-    if (personInDatabase.length > 0) {
-        return response.status(404).json({ message: `Person is already in the database` });
-    } else {
+  Person.find({ name: { $eq: body.name } })
+    .then((personInDatabase) => {
+      console.log(personInDatabase);
+      if (personInDatabase.length > 0) {
+        return response
+          .status(404)
+          .json({ message: `Person is already in the database` });
+      } else {
         const person = new Person({
-            name: body.name,
-            number: body.number,
-          });
-        
-          person.save().then((newPerson) => {
-            response.send(newPerson);
-          });
-    }
-  });
+          name: body.name,
+          number: body.number,
+        });
+
+        person.save().then((newPerson) => {
+          response.send(newPerson);
+        });
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
+app.delete("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id
 
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).send({ message: "User deleted" });
+  Person.findByIdAndRemove(id)
+    .then((result) => {
+      response.status(204).send({ message: "User deleted" });
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response) => {
   const body = request.body;
-  const id = Number(request.params.id);
+  const id = request.params.id
 
   // Check if its a valid user
   if (!body.name || !body.number) {
     response.status(404).send({ message: "invalid user " });
   }
 
-  currentPerson = persons.find((person) => person.id === id);
-  if (currentPerson) {
-    currentPerson.number = body.number;
-    persons = persons.map((person) =>
-      person.id === id ? currentPerson : person
-    );
-  } else {
-    return response.status(404).send({ message: "not a user" });
+  const person = {
+    name: body.name,
+    number: body.number
   }
 
-  response.send(currentPerson);
+  Person.findByIdAndUpdate(id, person, { new: true })
+  .then(newPerson => {
+    if (newPerson) {
+      response.send(newPerson)
+    }
+    else {
+      response.status(404).end();
+    }
+  })
+  .catch(error => next(error))
 });
+
+/*
+ * ERROR HANDLING MIDDLEWARE
+ */
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
